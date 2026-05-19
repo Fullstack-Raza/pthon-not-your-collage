143aa4c2feac0baacf03e32e117a975e10b3fd07
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-05-19 17:35:24 +0300

    operators

--- moew.py
+++ moew.py
@@ -1,4 +1,15 @@
-qw= "Muhammad"
-#sss= float(qw)
-zzz=bool(qw)
-print(zzz) 
+# name= input ("what is your name?")
+# age= input ("how old are you?")
+# age= int(age) 
+# print(f"  your half of age is {age/2}")
+# print(type(age)) 
+# print(f"5years ago your age was {age-5}")
+# a= int(input ("what number should be multiply by3"))
+
+# print (3*a)
+# print(3+2**2*5-1)
+# 4*5+3-1 
+
+print(15//4+15%4) 
+# (15/4)+(15%4)
+# 3+3
