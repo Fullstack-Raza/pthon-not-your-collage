33b8aa0a23c71c3eb0b60cc18848421d45753875
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-05-20 19:29:59 +0300

    if else

--- moew.py
+++ moew.py
@@ -1,15 +1,28 @@
 # name= input ("what is your name?")
 # age= input ("how old are you?")
 # age= int(age) 
-# print(f"  your half of age is {age/2}")
-# print(type(age)) 
-# print(f"5years ago your age was {age-5}")
-# a= int(input ("what number should be multiply by3"))
+#  print(f"  your half of age is {age/2}")
+#  print(type(age)) 
+#  print(f"5years ago your age was {age-5}")
+#  a= int(input ("what number should be multiply by3"))
 
-# print (3*a)
-# print(3+2**2*5-1)
-# 4*5+3-1 
+#  print (3*a)
+#  print(3+2**2*5-1)
+#  4*5+3-1 
 
-print(15//4+15%4) 
-# (15/4)+(15%4)
-# 3+3
+# print(15//4+15%4) 
+#  (15/4)+(15%4)
+#  3+3
+
+# arithmatic operator ----> + - / * % // ** 
+
+# commpression operator ----> < > <= >= ==  ans ----> True  , false
+# logiclal operator -----> and not or 
+# and  -----> aik bhi false asn false
+# or  ----> aik bhi true nas true
+# not  ----> ans ulta deta hy  agr true hy tu nas hoga flse
+
+# print(not 15>133 and not 22<15)
+
+
+
