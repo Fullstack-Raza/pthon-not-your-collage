6d36e8eba2ae4a4bc24c5ed3694042b5dd1c1579
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-05-21 19:07:46 +0300

    6q

--- if.py
+++ if.py
@@ -1,5 +1,73 @@
-a = int(input("age"))
-if a > 20 :
-    print('can')
-else:
-    print("not")
+# #  print greates number 
+# fristNUm=int(input("apka pehla number:-"))
+# secNUm=int(input("apka dosra number:-"))
+# if fristNUm==secNUm : 
+#     print("brabr hen")
+# elif fristNUm>secNUm: 
+#     print(fristNUm)
+# else :
+#     print(secNUm)
+
+# the_first_nmbr=int(input("Enter your first nmbr"))
+# the_second_nmbr=int(input("Enter your second nmbr"))
+# if the_first_nmbr>the_second_nmbr :
+#     print(the_first_nmbr)
+# elif the_first_nmbr<the_second_nmbr :
+#     print(the_second_nmbr)
+# else:
+#     print("both are equal")
+
+# a= input("say something")
+# if a=="m":
+#     print("good morning sir")
+# else:
+#     print("bad morning")
+
+
+# gen=input("please tell your gender:- (M for male F for female) ")
+# if gen=="M" or gen=="m" :
+#     print("Hello sir")
+# elif gen=="F" or gen=="f":
+#     print("Hello bitch")
+# else : print("invalid input")
+
+# num = int(input("Num:- "))
+# if num%2==0:print(f"{num} is even")
+# else :print(f"{num} is odd")
+
+
+
+
+
+
+
+# nim=int(input("tell me a fucking nmbbr i will tell you if its even or odd"))
+# if nim %2 ==0:
+#     print("ur fucking nmbr is even")
+# else: 
+#     print("ur fucking nmbr is odd nigga")
+
+
+
+
+
+
+
+# name=input("Name:- ")
+# age=int(input("Age:- "))
+# if age >=18:print(f"{name}u r valid for vote")
+
+# else:print(f"{name} u r not valid u can vote after {18-age} year")
+
+# year = int(input("give me year:- "))
+# if year%4==0 and year%400==0 : print("this is leap year")
+# else : print("its not")
+
+
+temprature=int(input("whats the temprature? "))
+if temprature<=5 and temprature>=-5:
+    print("freezing cold 🥶")
+elif temprature >=6 and temprature <=18:
+    print("pleasant 😊")
+elif temprature >=19 and temprature <=30:
+    print("very hot 🥵")
