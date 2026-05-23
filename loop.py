0f3bbc00b479551935af99475ad23982e5c8f25f
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-05-23 16:29:54 +0300

    break loop

--- loop.py
+++ loop.py
@@ -1,18 +1,43 @@
-num=int(input("num:- "))
-for i in range(10+1): 
-    print(num*i)
+# num=int(input("num:- "))
+# for i in range(10+1): 
+#     print(num*i)
 
 
 
-count= int(input("give me a nmbr:- "))
-for things in range (100,200):
-    print(count*things)
+# count= int(input("give me a nmbr:- "))
+# for things in range (100,200):
+#     print(count*things)
 
 
-name= "taqi"
-for i in range(len(name)):
-    print(name[i])
+# name= "taqi"
+# for i in range(len(name)):
+#     print(name[i])
 
 
-a="raza"
-print(len(a))
+# a="raza"
+# print(len(a))
+
+# for i in range(1,11,1):
+#     if i<=3 or i>8:
+#         continue
+#     print(i)    
+
+# for i in range(1,11,1):
+#     if i>=3 and i<8:
+#         continue
+#     print(i) 
+
+
+
+
+
+
+# word="hello world"
+# range3= int(input("how many time should i print: - "))
+# for i in range (1,range3+1,1):
+#     print (i)
+
+
+nmbr=int(input("tell me a nmr please"))
+for i in range(nmbr,0,-1):
+    print (i)
