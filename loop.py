a21e7d8d91f06d8125a1782461d6284f3f67b0f9
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-05-26 08:56:54 +0300

    numgame

--- loop.py
+++ loop.py
@@ -234,25 +234,6 @@
 
 
 
-import random
-randomNum= random.randint(1,100)
-UserNum=int(input("guess kron 0 sy 100 tk :- "))
-tryes=0
-if randomNum==UserNum:
-    tryes+=1
-    print(f"good game ap ny game ko khatam kra {tryes} gusses me ")
-else:
-    while randomNum!=UserNum:
-        tryes+=1
-        if UserNum<randomNum:
-            print("apka num low hy asl number sy ")
-            UserNum=int(input("guess kron 100 sy 0 tk :- "))
-        elif UserNum>randomNum:
-            print("apka num high hy asl num st ")
-            UserNum=int(input("guess kron 100 sy 0 tk :- "))
-    else :
-         tryes+=1
-         print(f"good game ap ny game ko khatam kra {tryes} gusses me ")
 
     
 
