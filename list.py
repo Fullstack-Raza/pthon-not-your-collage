1e41c6e96ff142ef39746e205e967ef10c303ccb
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-05-29 16:34:37 +0300

    solve list q

--- list.py
+++ list.py
@@ -32,15 +32,47 @@
 # mean /= len(numbers)
 # print(f"the avrage of ur numbers is: {mean}")
 
-l=[4,8,2,9,1]
-per=l[0]
-biggestnumindex=0
-for i in range(len(l)):
-    if l[i]>per:
-        biggestnumindex=i
-        per=l[i]
+# l=[4,8,2,9,1]
+# per=l[0]
+# biggestnumindex=0
+# for i in range(len(l)):
+#     if l[i]>per:
+#         biggestnumindex=i
+#         per=l[i]
 
-print(f"biggest number is:{per} and its index: {biggestnumindex}")
+# print(f"biggest number is:{per} and its index: {biggestnumindex}")
 
-    
+# l=[4,8,2,9,1] 
+# biggest=l[0]
+# big=l[0]
+
+# for i in l:
+#     if i> biggest:
+#         big=biggest
+#         biggest=i
+       
+      
+
+# print(f"secound big : {big}")
+# print(f"biggest : {biggest}")
+
+# l=[4,8,2,9,1] 
+# big=l[0]
+# secBig=l[0]
+
+# for i in l:
+#     if i>big:
+#         secBig=big
+#         big=i
+#     elif i>secBig:
+#         secBig=i
+# print(f"the sec big number is {secBig}")
+l=[1,3,5,7]
+
+for i in range(len(l)-1):
+    if l[i]> l[i+1]:
+        print("list is not sorted")
+        break
+else:
+    print("list is sorted")
 
