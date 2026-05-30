4b81211bcf66fa0b01bc14cdfe4d9adf7578042c
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-05-30 18:29:37 +0300

    tuple & set

---
+++ tuple.py
@@ -0,0 +1,12 @@
+# def student ():
+#     return "Taqy",24,"Gmail"
+
+# info = student()
+
+# naam , age , email = info #unpack tuple
+# print(naam)
+
+s1={10,20,30,40,50}
+s2={10,20}
+
+print(s1 > s2)
