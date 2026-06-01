cf84bb4db5843e90e035ddadf7a631e6a31f8054
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-06-01 19:24:51 +0300

    error handling

--- dictianry.py
+++ dictianry.py
@@ -1,7 +1,32 @@
-info ={"naam":"Raza","Email":"email","age":20}
-info1 ={"naame":"taqy","Emaili":"emailo","agea":30}
+# info ={"naam":"Raza","Email":"email","age":20}
+# info1 ={"naame":"taqy","Emaili":"emailo","agea":30}
 
-for i in info1:
-    info[i]=info1[i]
+# for i in info1:
+#     info[i]=info1[i]
 
-print(info) 
+# print(info) 
+
+# numbers={"a":10,"b":20,"c":30}
+# sum=0
+# for i in numbers:
+#     sum+=(numbers[i])
+# print(sum)
+
+# l=["a","a","a","a","a","a","a","b","b","b","b","b","b","b"]
+
+# d={}
+# for i in l:
+#     if i in d.keys():     
+#         d[i]+=1
+#     else:
+#         d[i]=1
+# print(d)
+
+a={"a":10,"b":30,"c":40}
+b={"c":40,"d":70}
+for i in b:
+    if i in  a.keys():
+        a[i]+=b[i]
+    else:
+        a[i]=b[i]
+print(a)
