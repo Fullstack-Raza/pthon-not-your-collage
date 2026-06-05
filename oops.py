d7f4d4967e3e2eb9b3083b6a29f235cf3e5ef4c3
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-06-05 16:40:24 +0300

    constoctor

--- oops.py
+++ oops.py
@@ -2,3 +2,17 @@
 # class
 # attribute
 # method
+
+
+class results:
+    def __init__(self,student, marks):
+        self.student= student
+        self.marks= marks
+mantiq =results("",100)
+masael =results("",98)
+qatar= results("",99)
+print (qatar.student,qatar.marks)
+print (masael.student,masael.marks)
+print (mantiq.student ,":",mantiq.marks)
+
+        
