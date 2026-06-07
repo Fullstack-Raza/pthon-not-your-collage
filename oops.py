a97281c1430aa860779e1f6cac81bf1317373977
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-06-07 18:07:57 +0300

    inharit in oops

--- oops.py
+++ oops.py
@@ -8,11 +8,20 @@ class results:
     def __init__(self,student, marks):
         self.student= student
         self.marks= marks
-mantiq =results("",100)
-masael =results("",98)
-qatar= results("",99)
-print (qatar.student,qatar.marks)
-print (masael.student,masael.marks)
-print (mantiq.student ,":",mantiq.marks)
+# mantiq =results("",100)
+# masael =results("",98)
+# qatar= results("",99)
+# print (qatar.student,qatar.marks)
+# print (masael.student,masael.marks)
+# print (mantiq.student ,":",mantiq.marks)
 
-        
+
+class finals (results):
+    def __init__(self, student, marks,finalMarks):
+        super().__init__(student, marks)
+        self.finalMarks= finalMarks
+    def show(self):
+        print ("your final mark is")
+        print(self.finalMarks)
+meow= finals("taqi",55,33)
+meow.show()
